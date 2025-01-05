e1055ab1085233a8df4bea15992f684ab7ac2bbd
shsato3199 <shsato2205@gmail.com>
2025-01-05 23:48:26 +0900

    検索結果を見るボタン押下時の遷移及び値を受け渡しが一応できたのでコミット

--- MyFridgeRecipes/MyFridgeRecipes/ContentView.swift
+++ MyFridgeRecipes/MyFridgeRecipes/ContentView.swift
@@ -1,45 +1,34 @@
-//
-//  ContentView.swift
-//  MyFridgeRecipes
-//
-//  Created by 佐藤翔馬 on 2025/01/01.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     @State private var searchResults: [String] = []  // 検索結果のデータ
-    @State private var isLinkActive: Bool = false     // 遷移の制御用フラグ
-    
+
     var body: some View {
         // NavigationStackでビューをラップ
         NavigationStack {
             VStack {
-                Button("検索") {
-                    // 検索アクションを実行
-                    performSearch()
-                    isLinkActive = true  // 検索後に遷移をトリガー
-                }
-                .padding()
-                .foregroundColor(.white)
-                .background(Color.blue)
-                .cornerRadius(10)
-                // 検索結果が表示されるビューに遷移
-                NavigationLink(
-                    value: searchResults,
-                    isActive: $isLinkActive){
-                    EmptyView()  // 遷移を発生させるためにはリンクを表示する必要がある
+                // NavigationLinkの新しい使い方
+                NavigationLink(value: searchResults) {
+                    Text("検索結果を見る")  // ボタンとして使うこともできる
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(Color.green)
+                        .cornerRadius(10)
+                        .onAppear {
+                            performSearch()
+                        }
                 }
                 .navigationDestination(for: [String].self) { results in
                     // 渡されたsearchResultsをRecipesViewに渡す
                     RecipesView(results: results)
                 }
+
                 NavigationLink("食材登録", destination: RecipeFoodRegisterView())
                     .padding()
                     .foregroundColor(.white)
                     .background(Color.blue)
                     .cornerRadius(10)
-                
+
                 NavigationLink("食材削除", destination: RecipeFoodDeleteView())
                     .padding()
                     .foregroundColor(.white)
@@ -49,6 +38,7 @@ struct ContentView: View {
             .navigationTitle("トップ画面")
         }
     }
+
     // 検索アクション
     func performSearch() {
         // ここで実際に検索を行う
